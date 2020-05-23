3bab2195aeaef9edffbff79820fdf43ddcd1b7d2
CallumREdwards <edwards.callumr@gmail.com>
2020-05-23 22:33:39 +0100

    Successful bigram generation

--- src/BigramGenerator.java
+++ src/BigramGenerator.java
@@ -1,24 +1,54 @@
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
 
-public class BigramGenerator implements Generator {
+public class BigramGenerator  {
+    HashMap<String, HashMap<String, Integer>> bigramFrequency = new HashMap<>();
+    HashMap<String, Integer> totalFrequency = new HashMap<>();    // Num of times this word comes before another
+
     public BigramGenerator(String[] trainingData) {
-
+        countFrequency(trainingData);
     }
 
-    private HashMap<String, HashMap<String, Integer>> countFrequency(String[] trainingData) {
-        HashMap<String, HashMap<String, Integer>> bigramFrequency = new HashMap<>();
+    private void countFrequency(String[] trainingData) {
         String previousWord = null;
         for (String word : trainingData) {
-            if (previousWord == null) continue;
-            HashMap<String, Integer> interMap = bigramFrequency.getOrDefault(previousWord, new HashMap<>());
-            interMap.put(word, 1 + interMap.getOrDefault(word, 0));
+
+            if (previousWord != null) {
+                HashMap<String, Integer> interMap = bigramFrequency.getOrDefault(previousWord, new HashMap<>());
+                interMap.put(word, 1 + interMap.getOrDefault(word, 0));
+                bigramFrequency.put(previousWord, interMap);
+                totalFrequency.put(previousWord, 1 + totalFrequency.getOrDefault(previousWord, 0));
+            }
+
             previousWord = word;
         }
-        return bigramFrequency;
     }
 
-    @Override
-    public String[] generate() {
+    public String[] generate(String initialWord, int length) {
+        String[] sentence =  new String[length];
+        String currentWord = initialWord;
+
+        for (int i=0; i<length; i++){
+            sentence[i] = currentWord;
+            currentWord = nextWord(currentWord);
+        }
+
+        return sentence;
+    }
+
+    private String nextWord(String currentWord){
+        Set<Map.Entry<String, Integer>> frequencySet = bigramFrequency.get(currentWord).entrySet();
+
+        int randomNum = ThreadLocalRandom.current().nextInt(1, totalFrequency.get(currentWord) + 1);
+
+        for (Map.Entry<String, Integer> entry : frequencySet) {
+            randomNum -= entry.getValue();
+            if (randomNum <= 0) return entry.getKey();
+        }
+
+        assert false;
         return null;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,31 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
     static final String TRAINING_PATH = "resources/alice.txt";
+    static final String INITIAL_WORD = "the".toLowerCase();
+    static final int SENTENCE_LENGTH = 12;
 
     public static void main(String[] args) {
         try {
-            String[] tokens = tokenise(new File(TRAINING_PATH));
+            String[] tokens = tokenize(new FileReader(TRAINING_PATH));
             BigramGenerator bigramGenerator = new BigramGenerator(tokens);
-        } catch (FileNotFoundException e) {
-            System.out.println("No file at " + TRAINING_PATH);
+            prettyPrint(bigramGenerator.generate(INITIAL_WORD, SENTENCE_LENGTH));
+        } catch (IOException e) {
+            System.out.println("File error at " + TRAINING_PATH);
             e.printStackTrace();
         }
     }
 
-    private static String[] tokenise(File file) throws FileNotFoundException {
-        Scanner scanner = new Scanner(file);
-        return scanner.next().split(" ");
+    private static void prettyPrint(String[] sentence) {
+        String output = String.join(" ", sentence);
+        output = output.substring(0, 1).toUpperCase() + output.substring(1) + ".";
+        System.out.println(output);
+    }
+
+    private static String[] tokenize(FileReader fileReader) throws IOException {
+        BufferedReader reader = new BufferedReader(fileReader);
+        return reader.readLine().split(" ");
     }
 }
